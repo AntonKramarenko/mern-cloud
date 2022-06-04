c43f374d34444db133b7b8341608b35c760d2aca
Anton <kramarenko4362@gmail.com>
2022-06-04 12:54:43 +0300

    deploy commit

--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ const mongoose = require('mongoose')
 const authRouter = require('./routes/auth.routes')
 const fileRouter = require('./routes/file.routes')
 const app = express()
-const PORT = config.get('serverPort') || 5000
+const PORT = process.env.PORT || config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware.js')
-
+const filePathMiddleware = require('./middleware/filepath.middleware.js')
+const path = require('path')
 
 
 app.use(fileUpload({}))
 app.use(corsMiddleware)
+app.use(filePathMiddleware(path.resolve(__dirname, 'files')))
 app.use(express.json())
 app.use(express.static('static'))
 app.use('/api/auth', authRouter)
